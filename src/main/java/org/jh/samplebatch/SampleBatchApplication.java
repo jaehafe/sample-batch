e46114ff2694da955afb3696776e8ad6a8795145
Jaeha Lee <wogkdkrm112@gmail.com>
2024-08-15 21:01:37 +0900

    feat: FirstSchedule

--- src/main/java/org/jh/samplebatch/SampleBatchApplication.java
+++ src/main/java/org/jh/samplebatch/SampleBatchApplication.java
@@ -3,8 +3,10 @@ package org.jh.samplebatch;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class SampleBatchApplication {
 
     public static void main(String[] args) {
