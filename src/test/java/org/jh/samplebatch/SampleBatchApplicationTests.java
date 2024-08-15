7b1f289b5c3df7188bff23d0eed0286ae7b38af6
Jaeha Lee <wogkdkrm112@gmail.com>
2024-08-15 15:53:29 +0900

    init: spring batch project

---
+++ src/test/java/org/jh/samplebatch/SampleBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package org.jh.samplebatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleBatchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
